638622c68462476c8dd810d89c99fb08b7758b94
Dave Kaplan <workalicious@gmail.com>
2013-02-04 13:12:16 -0600

    Initialized app for amdb, movie db, for class type-along project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviedb::Application.config.secret_token = '9ff24a352614f694db4a087d70c4edb0846b2cce5982d98da0a31030541742170d6f9253d425058529a0a6baf89bcd06a8fece8b8bf12577f65f2d5f7ec1e011'
